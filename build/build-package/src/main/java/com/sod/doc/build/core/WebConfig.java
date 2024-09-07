8bace192a0cfb61c6cd68cbaed452b2079b7c9e3
Prem Kumar Cherupally <premkumar@Prems-MacBook-Air.local>
2024-09-07 18:26:36 -0500

    added as a bean

--- build/build-package/src/main/java/com/sod/doc/build/core/WebConfig.java
+++ build/build-package/src/main/java/com/sod/doc/build/core/WebConfig.java
@@ -15,15 +15,10 @@ import java.util.List;
         public CorsFilter corsFilter() {
             UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
             CorsConfiguration config = new CorsConfiguration();
-
-            // Specify allowed origins explicitly; cannot use "*" with allowCredentials(true)
-            config.setAllowedOrigins(List.of("http://52.228.173.192")); // Add your frontend URL here
-
+            config.setAllowedOrigins(List.of("http://52.228.173.192"));
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "HEAD", "DELETE", "OPTIONS"));
             config.setAllowedHeaders(List.of("Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Authorization"));
             config.setExposedHeaders(List.of("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Authorization"));
-            config.setAllowCredentials(true); // Allows credentials
-
             source.registerCorsConfiguration("/**", config);
             return new CorsFilter(source);
         }
